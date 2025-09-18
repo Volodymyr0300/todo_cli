223b13e58bdc2c1afe0effb217c94ccd47052e64
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-17 23:28:58 -0500

    initial commit

---
+++ target/debug/deps/todo_cli-4c90ced160768c17.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/todo_cli/target/debug/deps/libtodo_cli-4c90ced160768c17.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/todo_cli/target/debug/deps/todo_cli-4c90ced160768c17.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/todo_cli-57d4770818563ce0.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/todo_cli/target/debug/deps/libtodo_cli-57d4770818563ce0.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/todo_cli/target/debug/deps/todo_cli-57d4770818563ce0.d: src/main.rs
+
+src/main.rs:
